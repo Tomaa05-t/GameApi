3596dd77e2ecef01e11a2b1492fffdb1a94f4cb2
Tomaa05-t <d.tomasini@studenti.unibg.it>
2026-04-15 15:03:02 +0200

    help

--- static/cards.js
+++ static/cards.js
@@ -1,5 +1,4 @@
-// js/cards.js - Versione Aggiornata per percorso 'carte/'
-
+// js/cards.js - Versione Completa con Punti
 const semiInfo = [
     { nome: 'bastoni', suffix: 'bast' },
     { nome: 'spade', suffix: 'spade' },
@@ -7,20 +6,26 @@ const semiInfo = [
     { nome: 'ori', suffix: 'ori' }     
 ];
 
+// Funzione per calcolare i punti della briscola
+function calcolaPunti(valore) {
+    if (valore === 1) return 11;
+    if (valore === 3) return 10;
+    if (valore === 10) return 4;
+    if (valore === 9) return 3;
+    if (valore === 8) return 2;
+    return 0;
+}
+
 const mazzo = [];
 
-// Generiamo il mazzo completo (40 carte)
 for (let seme of semiInfo) {
     for (let valore = 1; valore <= 10; valore++) {
         mazzo.push({
             seme: seme.nome,
             valore: valore,
+            punti: calcolaPunti(valore), // AGGIUNTO: Fondamentale per il server!
             id: `${valore}_${seme.suffix}`, 
-            // Percorso esatto: carte/bastoni/1_bast.png
             img: `carte/${seme.nome}/${valore}_${seme.suffix}.png` 
         });
     }
 }
-
-// Esportiamo il mazzo se necessario (o lo lasciamo globale)
-// window.mazzoCompleto = mazzo;
